318f48a49d2ffafecd8c147289a08966d6525454
Ferret <andrtazet@gmail.com>
2021-03-05 14:32:50 +0300

    parser

--- parser.py
+++ parser.py
@@ -8,7 +8,7 @@ import json
 
 
 class Parser:
-    def __init__(self, search_string, logger, tags:dict, pipe, processes=2, timeout=0.1):
+    def __init__(self, search_string, logger, tags: dict, pipe, processes=2, timeout=0.1):
         # получаем логгер
         self.logger = logger
 
@@ -23,8 +23,10 @@ class Parser:
         self.timeout = timeout
 
         self.session = requests.Session()
-        self.session.headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0', }
-        
+        self.session.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0', }
+        self.init_parser()
+
         # создаем теги для запроса
         self.tags = tags
         self.tags['searchString'] = search_string
@@ -34,39 +36,40 @@ class Parser:
         self.tags['recordsPerPage'] = '_500'
         self.tags['sortBy'] = 'UPDATE_DATE'
         self.tags['fz44'] = 'on'
-    
+
     def init_parser(self):
         self.session.get('https://zakupki.gov.ru/')
         self.logger.info('Парсер инициализирован')
-
 
     def start_async(self):
         process = mp.Process(target=self.async_parser)
         process.start()
         self.process = process
         return process
-    
+
     def async_parser(self):
         links = self.get_links_to_parse()
         self.parse_all(links)
 
     def get_links_to_parse(self) -> list:
         tags = self.tags
-        data = self.session.get('https://zakupki.gov.ru/epz/order/extendedsearch/results.html', params=tags)
+        data = self.session.get(
+            'https://zakupki.gov.ru/epz/order/extendedsearch/results.html', params=tags)
         data.raise_for_status()
 
         links = []
         # получаем количество страниц
-        page_num = int(document_fromstring(data.text).xpath('//ul[@class="pages"]/li[last()]/a/span/text()')[0])
+        page_num = int(document_fromstring(data.text).xpath(
+            '//ul[@class="pages"]/li[last()]/a/span/text()')[0])
         for i in range(1, page_num + 1):
             self.logger.info(f'получениие ссылок: {i}')
             page_data = self.session.get(self.parse_link, params=tags)
             page_data.raise_for_status()
-            links += list(map(lambda x: self.main_link + x, document_fromstring(page_data.text).xpath('//div[@class="registry-entry__header-mid__number"]/a/@href')))
+            links += list(map(lambda x: self.main_link + x, document_fromstring(
+                page_data.text).xpath('//div[@class="registry-entry__header-mid__number"]/a/@href')))
             tags['pageNumber'] = str(i)
             self.logger.info(f"загрузка ссылок. Страница {tags}")
         return links
-
 
     def parse_all(self, links):
         for link in links:
@@ -82,56 +85,65 @@ class Parser:
 
         # парсим главную информацию о закупке - номер цена заказчик дата
         card_info_container = order_document.cssselect('.cardMainInfo')[0]
-        tender_id = card_info_container.cssselect('.cardMainInfo__purchaseLink')
+        tender_id = card_info_container.cssselect(
+            '.cardMainInfo__purchaseLink')
 
         if not tender_id:
             tender_id = ''
         else:
             tender_id = self._normalizer(tender_id[0].text_content())
-        
-        tender_object = card_info_container.xpath('./div[1]/div[2]/div[1]/span[2]')
+
+        tender_object = card_info_container.xpath(
+            './div[1]/div[2]/div[1]/span[2]')
         if not tender_object:
             tender_object = ''
         else:
             tender_object = self._normalizer(tender_object[0].text_content())
-        
+
         customer = card_info_container.xpath('./div[1]/div[2]/div[2]/span[2]')
         if not customer:
             customer = ''
         else:
             customer = self._normalizer(customer[0].text_content())
-        
+
         tender_price = card_info_container.cssselect('.cost')
         if not tender_price:
             tender_price = ''
         else:
             tender_price = self._normalizer(tender_price[0].text_content())
-        
-        tender_date = card_info_container.xpath('./div[2]/div[2]/div[1]/div[1]/span[2]')
+
+        tender_date = card_info_container.xpath(
+            './div[2]/div[2]/div[1]/div[1]/span[2]')
         if not tender_date:
             tender_date = ''
         else:
             tender_date = self._normalizer(tender_date[0].text_content())
-        
-        # общая информация о закупке - адресс электронной площадки и обьект закупки
-        general_information_container = order_document.xpath('//div[@class="wrapper"]/div[2]/')
 
-        tender_adress = general_information_container.xpath('.//div[@class="col"]/section[3]/span[2]')
+        # общая информация о закупке - адресс электронной площадки и обьект закупки
+        general_information_container = order_document.xpath(
+            '//div[@class="wrapper"]/div[2]')
+
+        tender_adress = general_information_container[0].xpath(
+            './/div[@class="col"]/section[3]/span[2]')
         if not tender_adress:
             tender_adress = ''
         else:
             tender_adress = self._normalizer(tender_adress[0].text_content())
-        
+
         # условия контракта
-        condition_container = self._get_cotract_conditions_container(order_document.xpath('//div[@class="wrapper"]/div[@class="container"]'))
+        condition_container = self._get_cotract_conditions_container(
+            order_document.xpath('//div[@id="custReqNoticeTable"]/div'))
         if condition_container:
-            tender_delivery_adress = condition_container.xpath('.//div[@class="col"]/section[2]/span[2]')
+            tender_delivery_adress = condition_container.xpath(
+                './/div[@class="col"]/section[2]/span[2]')
             if not tender_delivery_adress:
                 tender_delivery_adress = ''
             else:
-                tender_delivery_adress = self._normalizer(tender_delivery_adress[0].text_content())
-            
-            tender_term = condition_container.xpath('.//div[@class="row"]/section[3]/span[2]')
+                tender_delivery_adress = self._normalizer(
+                    tender_delivery_adress[0].text_content())
+
+            tender_term = condition_container.xpath(
+                './/div[@class="row"]/section[3]/span[2]')
             if not tender_term:
                 tender_term = ''
             else:
@@ -139,7 +151,7 @@ class Parser:
         else:
             tender_delivery_adress = ''
             tender_term = ''
-        
+
         # парсинг информации о обьекте закупки
         tender_object_info = self._parse_tender_object_info(order_document)
 
@@ -147,61 +159,50 @@ class Parser:
         term_document_link = link.replace('common-info', 'documents')
         term_document_data = self.session.get(term_document_link)
         term_document_data.raise_for_status()
-        term_document_links = document_fromstring(term_document_data.text).xpath('//span[@class="section__value"]/a[@title]/@href')
+        term_document_links = document_fromstring(term_document_data.text).xpath(
+            '//span[@class="section__value"]/a[@title]/@href')
 
-        # парсинг результатов определения поставщика
-        term_result_link = link.replace('common-info', 'supplier-results')
-        term_result_data = self.session.get(term_result_link)
-        term_result_data.raise_for_status()
-        term_results_doc = document_fromstring(term_result_data.text)
+        return {
+            'tender_id': tender_id, 'tender_object': tender_object, 'customer': customer,
+            'tender_price': tender_price, 'tender_adress': tender_adress, 'tender_delivery': tender_delivery_adress,
+            'tender_term': tender_term, 'tender_object_info': tender_object_info, 'document_links': term_document_links,
+            'type': 'fz44', 'link': link
+        }
 
-        
-        
-
-        
-    
     def _parse_tender_object_info(self, document):
         # получаем контейнер таблицы
-        table = document.xpath('//div[@id]/table[@class="blockInfo__table tableBlock"]')
+        table = document.xpath(
+            '//div[@id]/table[@class="blockInfo__table tableBlock"]')
         if not table:
             return json.dumps([])
         else:
             table = table[0]
-        
+
         # получаем и обрабатываем заголовки
         headers_raw = table.xpath('./thead/tr[1]/th/text()')
         headers = list(map(self._normalizer, headers_raw))
 
         # данные таблицы
-        data = [list(map(self._normalizer, row.xpath('./td/text()')))  for row in table.xpath('./tbody/tr[@class="tableBlock__body"]')]
+        data = [list(map(self._normalizer, row.xpath('./t/td/text()')))
+                for row in table.xpath('./tbody/tr[@class="tableBlock__body"]')]
 
         # создание json документа
-        json_data = json.dumps(headers + data)
+        json_data = json.dumps(data, ensure_ascii=False)
 
         return json_data
 
-    
     def _get_cotract_conditions_container(self, containers):
-        for element in range(len(containers)):
-            name = element.xpath('.//h2').text_content()
-            if 'Условия контракта' in self._normalizer(name):
+        for element in containers:
+            name = element.xpath('.//h2')[0].text_content()
+            print(self._normalizer(name))
+            if 'Условия' in self._normalizer(name):
                 return element
         return False
-
 
     def _normalizer(self, text: str) -> str:
         return text.replace('\n', '').replace('\xa0', '').rstrip().lstrip()
 
 
-if __name__ == "__main__":
-    logger = logging.getLogger()
-    parent_pipe, child_pipe = mp.Pipe()
-    parser = Parser('техно', logger, {'pc': 'on'}, child_pipe)
-    process = parser.start_async()
-
-    while True:
-        data = parent_pipe.recv()
-        if 'end' in data:
-            break
-        else:
-            print(data)
+parser = Parser('техно', logging.getLogger(), {'pc': 'on'}, mp.Pipe()[1])
+data = parser.parse_ea44('https://zakupki.gov.ru/epz/order/notice/ea44/view/common-info.html?regNumber=0188300007521000001')
+print(json.dumps(data, ensure_ascii=False), file=open('test.json', 'w'))
